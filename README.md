56324d9816205ffdd69b5d6ace05e2e58b297db9
Volodymyr Taliar <vtaliar@blackberry.com>
2020-12-14 22:41:54 +0200

    cordova-plugin-bbd-file initial commit

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# BlackBerry-Dynamics-Cordova-Plugins
+# cordova-plugin-bbd-file
+
+BlackBerry Dynamics Cordova File plugin. It is a fork of [cordova-plugin-file@6.0.1](https://github.com/apache/cordova-plugin-file). 
+
+This plugin enables you to manage the FileSystem residing within the BlackBerry Dynamics secure container using a similar JavaScript API to the original plugin.
+
+## Preconditions
+`cordova-plugin-bbd-file` is dependent on `cordova-plugin-bbd-base` plugin.
+
+Installation
+============
+To add this plugin to your application, run the following command in the project directory:
+```
+$ cd <path/to/package>/BlackBerry_Dynamics_SDK_for_Cordova_<version>/plugins/cordovaApp
+$ cordova plugin add ../cordova-plugin-bbd-file
+```
+
+## Supported Platforms
+
+- Android
+- iOS
+
+## File System Layouts
+It can be very useful to know how the `cordova.file.*` properties map to physical paths on a real device.
+
+##### iOS 
+On iOS the `cordova-plugin-bbd-file` plugin supports all constants listed [here](https://cordova.apache.org/docs/en/latest/reference/cordova-plugin-file/#ios-file-system-layout).
+
+##### Android
+On Android the `cordova-plugin-bbd-file` plugin supports all constants listed [here](https://cordova.apache.org/docs/en/latest/reference/cordova-plugin-file/#android-file-system-layout) except the following:
+- `externalRootDirectory`
+- `externalApplicationStorageDirectory`
+- `externalCacheDirectory`
+- `externalDataDirectory`
+
+## Usage
+
+Please take a look at the original examples [here](https://github.com/apache/cordova-plugin-file#sample-create-files-and-directories-write-read-and-append-files-).
+
+## File + AppKinetics
+
+The BlackBerry Dynamics Cordova File plugin is compatible with other plugins. One great example is the AppKinetics plugin.
+
+BlackBerry Dynamics Cordova AppKinetics plugin provides `storageLocation` constant that allows to resolve files that were transfered from other Dynamics apps using Dynamics app-based [services](https://marketplace.blackberry.com/services) like [transfer-file](https://marketplace.blackberry.com/services/273065) or were copied from application bundle to Dynamics secure container using `copyFilesToSecureFileSystem()` API.
+
+Details about the AppKinetics module can be found [here](https://developer.blackberry.com/devzone/files/blackberry-dynamics/cordova/GDAppKinetics.html).
+
+#### Example - resolving AppKinetics storage
+```javascript
+resolveLocalFileSystemURL(window.plugins.GDAppKineticsPlugin.storageLocation,
+    function(directoryEntry) {
+        var appKineticsDirectoryReader = directoryEntry.createReader();
+            appKineticsDirectoryReader.readEntries(function (entries) {
+            console.log("AppKinetics storage entries: ", entries);
+        }, function(error) {
+            console.log("Error: ", error);
+        });
+    }, function(error) {
+        console.log("resolveLocalFileSystemURL error: ", error)
+    }
+ );
+```
+
+## Dynamics logs
+> The BlackBerry Dynamics Cordova File plugin has API to work with Dynamics logs.
+
+- `FileSystem.uploadLogs` API allows to upload BlackBerry Dynamics activity logs for support purposes. The logs will be uploaded to a server in the BlackBerry Technology Network Operation Center (NOC). Upload takes place in background and is retried as necessary.
+- `FileSystem.exportLogFileToDocumentsFolder` API allows to create a dump of BlackBerry Dynamics activity logs. The logs will be dumped to a file that is outside the secure store, in the `Documents` folder. The file will not be encrypted.
+
+#### Example - uploadLogs
+
+```javascript
+requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+    fileSystem.uploadLogs(function() {
+        console.log("Logs are uploaded to the NOC");
+    }, null);
+}, null);
+```
+
+#### Example - exportLogFileToDocumentsFolder
+
+```javascript
+requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+    fileSystem.exportLogFileToDocumentsFolder(function() {
+        console.log("Logs are exported to the Documents folder");
+    }, null);
+}, null);
+```
---
+++ src/ios/CDVFileBD.h
@@ -0,0 +1,163 @@
+/*
+ Copyright (c) 2020 BlackBerry Limited. All Rights Reserved.
+ Some modifications to the original Cordova File plugin
+ from https://github.com/apache/cordova-plugin-file/
+ 
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+
+extern NSString* const kCDVAssetsLibraryPrefixBD;
+extern NSString* const kCDVFilesystemURLPrefixBD;
+
+enum CDVFileErrorBD {
+    NO_ERROR = 0,
+    NOT_FOUND_ERR = 1,
+    SECURITY_ERR = 2,
+    ABORT_ERR = 3,
+    NOT_READABLE_ERR = 4,
+    ENCODING_ERR = 5,
+    NO_MODIFICATION_ALLOWED_ERR = 6,
+    INVALID_STATE_ERR = 7,
+    SYNTAX_ERR = 8,
+    INVALID_MODIFICATION_ERR = 9,
+    QUOTA_EXCEEDED_ERR = 10,
+    TYPE_MISMATCH_ERR = 11,
+    PATH_EXISTS_ERR = 12
+};
+typedef int CDVFileErrorBD;
+
+@interface CDVFilesystemURLBD : NSObject  {
+    NSURL *_url;
+    NSString *_fileSystemName;
+    NSString *_fullPath;
+}
+
+- (id) initWithString:(NSString*)strURL;
+- (id) initWithURL:(NSURL*)URL;
++ (CDVFilesystemURLBD *)fileSystemURLWithString:(NSString *)strURL;
++ (CDVFilesystemURLBD *)fileSystemURLWithURL:(NSURL *)URL;
+
+- (NSString *)absoluteURL;
+
+@property (atomic) NSURL *url;
+@property (atomic) NSString *fileSystemName;
+@property (atomic) NSString *fullPath;
+
+@end
+
+@interface CDVFilesystemURLProtocolBD : NSURLProtocol
+@end
+
+@protocol CDVFileSystemBD
+- (CDVPluginResult *)entryForLocalURI:(CDVFilesystemURLBD *)url;
+- (CDVPluginResult *)getFileForURL:(CDVFilesystemURLBD *)baseURI requestedPath:(NSString *)requestedPath options:(NSDictionary *)options;
+- (CDVPluginResult *)getParentForURL:(CDVFilesystemURLBD *)localURI;
+- (CDVPluginResult *)setMetadataForURL:(CDVFilesystemURLBD *)localURI withObject:(NSDictionary *)options;
+- (CDVPluginResult *)removeFileAtURL:(CDVFilesystemURLBD *)localURI;
+- (CDVPluginResult *)recursiveRemoveFileAtURL:(CDVFilesystemURLBD *)localURI;
+- (CDVPluginResult *)readEntriesAtURL:(CDVFilesystemURLBD *)localURI;
+- (CDVPluginResult *)truncateFileAtURL:(CDVFilesystemURLBD *)localURI atPosition:(unsigned long long)pos;
+- (CDVPluginResult *)writeToFileAtURL:(CDVFilesystemURLBD *)localURL withData:(NSData*)encData append:(BOOL)shouldAppend;
+- (void)copyFileToURL:(CDVFilesystemURLBD *)destURL withName:(NSString *)newName fromFileSystem:(NSObject<CDVFileSystemBD> *)srcFs atURL:(CDVFilesystemURLBD *)srcURL copy:(BOOL)bCopy callback:(void (^)(CDVPluginResult *))callback;
+- (void)readFileAtURL:(CDVFilesystemURLBD *)localURL start:(NSInteger)start end:(NSInteger)end callback:(void (^)(NSData*, NSString* mimeType, CDVFileErrorBD))callback;
+- (void)getFileMetadataForURL:(CDVFilesystemURLBD *)localURL callback:(void (^)(CDVPluginResult *))callback;
+
+- (NSDictionary *)makeEntryForLocalURL:(CDVFilesystemURLBD *)url;
+- (NSDictionary*)makeEntryForPath:(NSString*)fullPath isDirectory:(BOOL)isDir;
+
+@property (nonatomic,strong) NSString *name;
+@property (nonatomic, copy) NSURL*(^urlTransformer)(NSURL*);
+
+@optional
+- (NSString *)filesystemPathForURL:(CDVFilesystemURLBD *)localURI;
+- (CDVFilesystemURLBD *)URLforFilesystemPath:(NSString *)path;
+
+@end
+
+@interface CDVFileBD : CDVPlugin {
+    NSString* rootDocsPath;
+    NSString* appDocsPath;
+    NSString* appLibraryPath;
+    NSString* appTempPath;
+
+    NSMutableArray* fileSystems_;
+    BOOL userHasAllowed;
+}
+
+- (NSNumber*)checkFreeDiskSpace:(NSString*)appPath;
+- (NSDictionary*)makeEntryForPath:(NSString*)fullPath fileSystemName:(NSString *)fsName isDirectory:(BOOL)isDir;
+- (NSDictionary *)makeEntryForURL:(NSURL *)URL;
+- (CDVFilesystemURLBD *)fileSystemURLforLocalPath:(NSString *)localPath;
+
+- (NSObject<CDVFileSystemBD> *)filesystemForURL:(CDVFilesystemURLBD *)localURL;
+
+/* Native Registration API */
+- (void)registerFilesystem:(NSObject<CDVFileSystemBD> *)fs;
+- (NSObject<CDVFileSystemBD> *)fileSystemByName:(NSString *)fsName;
+
+/* Exec API */
+- (void)requestFileSystem:(CDVInvokedUrlCommand*)command;
+- (void)resolveLocalFileSystemURI:(CDVInvokedUrlCommand*)command;
+- (void)getDirectory:(CDVInvokedUrlCommand*)command;
+- (void)getFile:(CDVInvokedUrlCommand*)command;
+- (void)getParent:(CDVInvokedUrlCommand*)command;
+- (void)removeRecursively:(CDVInvokedUrlCommand*)command;
+- (void)remove:(CDVInvokedUrlCommand*)command;
+- (void)copyTo:(CDVInvokedUrlCommand*)command;
+- (void)moveTo:(CDVInvokedUrlCommand*)command;
+- (void)getFileMetadata:(CDVInvokedUrlCommand*)command;
+- (void)readEntries:(CDVInvokedUrlCommand*)command;
+- (void)readAsText:(CDVInvokedUrlCommand*)command;
+- (void)readAsDataURL:(CDVInvokedUrlCommand*)command;
+- (void)readAsArrayBuffer:(CDVInvokedUrlCommand*)command;
+- (void)write:(CDVInvokedUrlCommand*)command;
+- (void)testFileExists:(CDVInvokedUrlCommand*)command;
+- (void)testDirectoryExists:(CDVInvokedUrlCommand*)command;
+- (void)getFreeDiskSpace:(CDVInvokedUrlCommand*)command;
+- (void)truncate:(CDVInvokedUrlCommand*)command;
+- (void)doCopyMove:(CDVInvokedUrlCommand*)command isCopy:(BOOL)bCopy;
+- (void)exportLogFileToDocumentsFolder:(CDVInvokedUrlCommand *)command;
+- (void)uploadLogs:(CDVInvokedUrlCommand*)command;
+
+/* Compatibilty with older File API */
+- (NSString*)getMimeTypeFromPath:(NSString*)fullPath;
+- (NSDictionary *)getDirectoryEntry:(NSString *)target isDirectory:(BOOL)bDirRequest;
+
+/* Conversion between filesystem paths and URLs */
+- (NSString *)filesystemPathForURL:(CDVFilesystemURLBD *)URL;
+
+/* Internal methods for testing */
+- (void)_getLocalFilesystemPath:(CDVInvokedUrlCommand*)command;
+
+@property (nonatomic, strong) NSString* rootDocsPath;
+@property (nonatomic, strong) NSString* appDocsPath;
+@property (nonatomic, strong) NSString* appLibraryPath;
+@property (nonatomic, strong) NSString* appTempPath;
+@property (nonatomic, strong) NSString* persistentPath;
+@property (nonatomic, strong) NSString* temporaryPath;
+@property (nonatomic, strong) NSMutableArray* fileSystems;
+
+@property BOOL userHasAllowed;
+
+@end
+
+#define kW3FileTemporary @"temporary"
+#define kW3FilePersistent @"persistent"
